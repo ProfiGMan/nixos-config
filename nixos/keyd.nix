7a439cb463e184ff048521a3baa21e9b3223f86b
profi_gman <profi_gman@outlook.com>
2024-08-29 00:12:19 +0300

    upd

--- nixos/keyd.nix
+++ nixos/keyd.nix
@@ -2,14 +2,10 @@
   services.keyd = {
     enable = true;
     keyboards = {
-      # The name is just the name of the configuration file, it does not really matter
       default = {
-        ids = [ "*" ]; # what goes into the [id] section, here we select all keyboards
-        # Everything but the ID section:
+        ids = [ "*" ]; 
         settings = {
-          # The main layer, if you choose to declare it in Nix
           main = {
-            # capslock = "layer(control)"; # you might need to also enclose the key in quotes if it contains non-alphabetical symbols
 	    tab = "overload(vimAnywhere, tab)";
 	    capslock = "overload(meta, esc)";
 	    space = "overloadt(control, space, 300)";
@@ -25,11 +21,13 @@
 	    u = "pageup";
 	    d = "pagedown";
 	    e = "tab";
+	    "0" = "home";
+	    "shift+4" = "end";
+	  };
+	  "vimAnywhere+shift" = {
+	    "4" = "end";
 	  };
         };
-        extraConfig = ''
-          # put here any extra-config, e.g. you can copy/paste here directly a configuration, just remove the ids part
-        '';
       };
     };
   };
