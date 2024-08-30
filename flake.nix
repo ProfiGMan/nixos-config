b708572e9b8b2189aa84f96a0feb19e47fad8d79
Profi_GMan <profi_gman@outlook.com>
2024-08-31 00:46:13 +0300

    upd

--- flake.nix
+++ flake.nix
@@ -1,78 +1,78 @@
 {
 
-	description = "Best configuration";
+  description = "Best configuration";
 
-	inputs = {
-		# nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-		# nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		# nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-		nixpkgs-unfree.url = "github:numtide/nixpkgs-unfree";
-		nixpkgs-unfree.inputs.nixpkgs.follows = "nixpkgs";
-                nur.url = "github:nix-community/NUR";
+  inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unfree.url = "github:numtide/nixpkgs-unfree";
+    nixpkgs-unfree.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = "github:nix-community/NUR";
 
 
-		home-manager = {
-			# url = "github:nix-community/home-manager/release-24.05";
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    home-manager = {
+      # url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-		nixvim = {
-		  url = "github:nix-community/nixvim";
-		  inputs.nixpkgs.follows = "nixpkgs";
-		};
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-		firefox-addons = {
-      			url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      			inputs.nixpkgs.follows = "nixpkgs";
-    		};
-	};
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+  };
 
-	outputs = { nixpkgs, nixpkgs-unfree, home-manager, ... }@inputs:
-		let
-			system = "x86_64-linux";
-			pkgs = import nixpkgs {
-			  inherit system;
-			  config = {
-			    allowUnfree = true;
-			    allowUnfreePredicate = _: true;
-			  };
-			};
-			# pkgs-unfree = import nixpkgs-unfree {
-			  # inherit system;
-			# };
-		in {
-		nixosConfigurations.BoberPC = nixpkgs.lib.nixosSystem {
-			inherit system;
-			# specialArgs = { inherit inputs; pkgs = pkgs; };
-			specialArgs = {
-			  inherit inputs;
-			  inherit nixpkgs-unfree;
-			  # pkgs-stable = import nixpkgs-stable {
-			  #   inherit system;
-			  #   config.allowUnfree = true;
-			  # };
-			  pkgs-unfree = import nixpkgs-unfree {
-			    inherit system;
-			    config.allowUnfree = true;
-			  };
-			};
-			modules = [
-                          ./nixos/configuration.nix
-			  inputs.nixvim.nixosModules.nixvim
-			];
-		};
+  outputs = { nixpkgs, nixpkgs-unfree, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = _: true;
+        };
+      };
+      # pkgs-unfree = import nixpkgs-unfree {
+        # inherit system;
+      # };
+    in {
+    nixosConfigurations.BoberPC = nixpkgs.lib.nixosSystem {
+      inherit system;
+      # specialArgs = { inherit inputs; pkgs = pkgs; };
+      specialArgs = {
+        inherit inputs;
+        inherit nixpkgs-unfree;
+        # pkgs-stable = import nixpkgs-stable {
+        #   inherit system;
+        #   config.allowUnfree = true;
+        # };
+        pkgs-unfree = import nixpkgs-unfree {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
+      modules = [
+        ./nixos/configuration.nix
+        inputs.nixvim.nixosModules.nixvim
+	home-manager.nixosModules.home-manager
+      ];
+    };
 
-		homeConfigurations.bober = home-manager.lib.homeManagerConfiguration {
-			pkgs = nixpkgs.legacyPackages.${system};
-
-			extraSpecialArgs = { inherit inputs; };
-
-		 	modules = [ 
-		 	  ./home-manager/home.nix ];
-
-		};
-	};
+#     homeConfigurations.bober = home-manager.lib.homeManagerConfiguration {
+#       pkgs = nixpkgs.legacyPackages.${system};
+# 
+#       extraSpecialArgs = { inherit inputs; };
+# 
+#       modules = [ ./home-manager/home.nix ];
+# 
+#     };
+  };
 
 }
--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -4,6 +4,7 @@
     enable = true;
     shellAliases = {
       v = "nvim";
+      cat = "bat";
       re = "sudo nixos-rebuild switch --flake ~/nix";
       hre = "home-manager switch --flake ~/nix";
     };
